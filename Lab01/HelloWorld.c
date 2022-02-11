c01e82b4d92533b6f6f0f0b6940d9a9647a266fd
Guilherme Santos <guilherme-santos2013@hotmail.com>
2022-02-11 09:18:29 -0300

    HelloWorld Completo

--- Lab01/HelloWorld.c
+++ Lab01/HelloWorld.c
@@ -1,9 +1,9 @@
 /*
     NOME: Victor De Marqui - TIA: 32014287
-
+    NOME: Guilherme Rodrigues Santos - TIA: 32012209
 */
-
+#include <stdio.h>
 int main(){
     printf("Hello World");
-
+    return 0;
 }
