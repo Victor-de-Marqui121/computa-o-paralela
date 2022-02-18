76b349ed7aea3d0f4f6c5f7cd794e9a7a6f65548
Victor-de-Marqui121 <vitudm588@gmail.com>
2022-02-18 09:57:48 -0300

    Update exercicio13.c

--- Lab02/exercicio13.c
+++ Lab02/exercicio13.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
 
+int main() {
+    /*13. Faça um rotina que remova um caracter de uma string do tipo char Str[100], dada a posição do caracter.*/
+    char str[100] = "";
+    int pos;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", &str);
+
+    printf("Digite a posicao para a letra ser removida: ");
+    scanf(" %d", &pos);
+
+    if ((pos) <= strlen(str)) strcpy(&str[pos], &str[1 + pos]);
+
+    printf("\nNova String: %s", str);
+    return 0;
+}
