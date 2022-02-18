34c8cfdd8f01a41187ecd1bcbf1a756775d1deac
Victor-de-Marqui121 <90067572+Victor-de-Marqui121@users.noreply.github.com>
2022-02-18 13:37:18 -0300

    Update exercio4.c

--- Lab02/exercio4.c
+++ Lab02/exercio4.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<string.h>
 
+int main(){
+   char nomes[2][100];
+   char aux[100];
+
+    for(int i = 0; i < 2; i++){
+        printf("Digite o %d* nome: ", i+1);
+        gets(nomes[i]);
+    }
+
+    for(int i = 0; i < 2; i++){
+        for(int j = i + 1; j < 2; j++){
+            if(strcmp(nomes[i], nomes[j]) > 0){
+                strcpy(aux, nomes[i]);
+                strcpy(nomes[i], nomes[j]);
+                strcpy(nomes[j], aux);
+            }
+        }
+    }
+
+    printf("\nNomes Ordenados:\n");
+    for(int i = 0; i < 2; i++) printf("+ %s\n", nomes[i]);
+    return 0;
+}
