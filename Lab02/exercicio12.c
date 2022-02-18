2cbb3fe850eaba325d2e6a89723048344e389f8e
Victor-de-Marqui121 <vitudm588@gmail.com>
2022-02-18 09:43:52 -0300

    Update exercicio12.c

--- Lab02/exercicio12.c
+++ Lab02/exercicio12.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+int main() {
+    /*12. Faça uma função que retorne a posição de um dado caracter dentro de uma string.*/
+    char str[100] = "";
+    char c;
+
+    printf("Digite uma palavra: ");
+    scanf("%s", &str);
+
+    printf("Digite uma letra para buscar: ");
+    scanf(" %c", &c);
+
+    printf("O nome armazenado foi: %s ", str, strlen(str));
+
+    for (int i = 0; i < strlen(str) + 1; i++)
+        (str[i] == c) ? printf("\n+ Caractere %c encontrado na posicao -> %d ", c, i) : printf("");
+
+    return 0;
+}
 
