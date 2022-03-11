095343963126e9965704a44b5e6bf72efdae5cd1
Victor-de-Marqui121 <90067572+Victor-de-Marqui121@users.noreply.github.com>
2022-03-11 12:46:03 -0300

    Update Codigo.c

--- Lab04 Structs/Codigo.c
+++ Lab04 Structs/Codigo.c
@@ -9,56 +9,67 @@ typedef struct{
 }PESSOA;
 
 void printa(PESSOA *Povo){
-  printf("> Nome\t Idade\t Altura <\n");
-  for (int i = 0; i < 10; i++){
-      printf("%s - %d - %2.2f\n", Povo[i].Name,Povo[i].Idade,Povo[i].Altura );
-  }
+    printf("> Nome\t Idade\t Altura <\n");
+    for (int i = 0; i < 10; i++){
+        printf("%s - %d - %2.2f\n", Povo[i].Name,Povo[i].Idade,Povo[i].Altura );
+    }
 }
 
 int compara(const void *a, const void *b)
 {
-  const PESSOA *A = a;
-  const PESSOA *B = b;
+    const PESSOA *A = a;
+    const PESSOA *B = b;
 
-  if(A->Altura == B->Altura) return 0;
-  else if (A->Altura > B->Altura) return -1;
-  else return 1;
+    if(A->Altura == B->Altura) return 0;
+    else if (A->Altura > B->Altura) return -1;
+    else return 1;
 }
 
 int main(void){
-  PESSOA Povo[10];
-  FILE *Arq;
+    PESSOA Povo[10];
+    FILE *Arq;
 
-  ssize_t read;
-  size_t len;
-  char *line = NULL;
-      
-  if ((Arq = fopen("infoSample.txt", "r"))) {
+    ssize_t read;
+    size_t len;
+    char *line = NULL;
     int index = 0;
-    while ((read = getline(&line, &len, Arq)) != -1) 
-    {
-      char *tempAux = strtok(line, ";");
-      strcpy(Povo[index].Name, tempAux);
-      
-      tempAux = strtok(NULL, ";");
-      Povo[index].Idade = atoi(tempAux);
-      
-      tempAux = strtok(NULL, ";");
-      Povo[index].Altura = atof(tempAux);
-      
-      index++;
-    }    
-  }
 
-  printf("Antes da Ordenação: \n");
-  (printa(Povo));
-  qsort (Povo, 10, sizeof(PESSOA), compara);
-  printf("\n\nDepois da Ordenação: \n");
-  (printa(Povo));
-  
-  fclose(Arq);
-  if (line)
-    free(line);
-  
-  return 0;
+    if ((Arq = fopen("infoSample.txt", "r"))) {
+        while ((read = getline(&line, &len, Arq)) != -1) 
+        {
+            char *tempAux = strtok(line, ";");
+            strcpy(Povo[index].Name, tempAux);
+            
+            tempAux = strtok(NULL, ";");
+            Povo[index].Idade = atoi(tempAux);
+            
+            tempAux = strtok(NULL, ";");
+            Povo[index].Altura = atof(tempAux);
+            
+            index++;
+        }    
+    }
+
+    printf("Antes da Ordenação: \n");
+    (printa(Povo));
+
+
+    qsort (Povo, 10, sizeof(PESSOA), compara);
+
+    printf("\n\nDepois da Ordenação: \n");
+    (printa(Povo));
+
+    fclose(Arq);
+    Arq = fopen("OrderedSample.txt", "w");
+
+    for(int i = 0; i < 10; i++)    {
+        fprintf(Arq,"%s;%d;%2.2f\n",Povo[i].Name,Povo[i].Idade,Povo[i].Altura );
+    }
+
+    if (line){
+        free(line);
+    }
+        
+    fclose(Arq);
+    return 0;
 }
