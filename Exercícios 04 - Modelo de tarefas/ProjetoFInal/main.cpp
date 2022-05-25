92c1b6fcfe6c2b82c0f8719d162739bed793072f
Victor-de-Marqui121 <90067572+Victor-de-Marqui121@users.noreply.github.com>
2022-05-25 14:57:17 -0300

    Create main.cpp

---
+++ Exercícios 04 - Modelo de tarefas/ProjetoFInal/main.cpp
@@ -0,0 +1,25 @@
+// Victor De Marqui 32014287
+// Giovanni Vichiatti Mori 32054033
+// Guilherme Rodrigues Santos 32012209
+#include <omp.h>
+#include <stdio.h>
+
+int main()
+{
+   long double serie;
+   long double T = 1000000000;
+
+   #pragma omp parallel 
+   {
+      long double S = 0;
+      
+      #pragma omp for schedule(dynamic) nowait
+      for (int i = 1; i <= T ; i++)
+      {
+         S += (1.0/i);
+      }
+      #pragma omp critical
+      serie += S; 
+   }
+   printf("Serie de Taylor(%Lf): %Lf\n", T, serie);
+}
