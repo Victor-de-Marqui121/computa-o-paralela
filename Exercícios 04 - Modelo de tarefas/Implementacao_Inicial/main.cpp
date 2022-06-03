b3c54fdc2f02bf4eab8b4ac44b4c28711ac83f76
Victor-de-Marqui121 <90067572+Victor-de-Marqui121@users.noreply.github.com>
2022-06-03 10:15:34 -0300

    Update main.cpp

--- Exercícios 04 - Modelo de tarefas/Implementacao_Inicial/main.cpp
+++ Exercícios 04 - Modelo de tarefas/Implementacao_Inicial/main.cpp
@@ -1,15 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main() {
-  int T = 10;
-  float S = 0;
+  long long int T = 2222222222;
+  double S = 0;
 	
-	for(float i = 1; i <= T; i++)
+	for(long long int i = 1; i <= T; i++)
 	{
-		cout << "S = " << S << " + " << (1/i) << " = " << (S + (1/i)) << endl;
-		S += (1/i);
+      //if (i%10000 == 1)cout << "S = " << S << " + " << (1/i) << " = " << (S + (1/i)) << endl;
+		S += (1/(float)i);
 	}
-	cout << "Serie de Taylor(" << T  << "): " << S;
+	printf("Serie de Taylor(%lld): %f", T, S);
 	return 0;
 }
